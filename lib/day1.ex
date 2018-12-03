b5ab976295f8eaf806f52da1fd8c1502bf113c60
Brian Balser <brian.balser@gmail.com>
2018-12-02 20:01:10 -0500

    Day1 - part 2

--- lib/day1.ex
+++ lib/day1.ex
@@ -2,9 +2,27 @@ defmodule Day1 do
 
   def part_one(input) do
     String.split(input, ~r/\n/)
-    |> Enum.map(fn number when number == "" -> 0
-                   number -> String.to_integer(number) end)
+    |> Enum.map(&to_integer/1)
     |> Enum.sum()
   end
+
+  def part_two(input) do
+    {:halted, answer} = String.split(input, ~r/\n/)
+    |> Enum.map(&to_integer/1)
+    |> Stream.cycle()
+    |> Stream.filter(fn number -> number != 0 end)
+    |> Enumerable.reduce({:cont, {0, []}}, fn next, {total, list} ->
+      new_total = total + next
+      case new_total in list do
+        true -> {:halt, new_total}
+        false -> {:cont, {new_total, [new_total | list]}}
+      end
+    end)
+
+    answer
+  end
+
+  defp to_integer(number) when number == "", do: 0
+  defp to_integer(number), do: String.to_integer(number)
 
 end
--- test/day1_test.exs
+++ test/day1_test.exs
@@ -11,4 +11,16 @@ defmodule Day1Tests do
     assert Day1.part_one(input) == 19
   end
 
+  test "part_two detect first double frequency" do
+    input = """
+    3
+    3
+    4
+    -2
+    -4
+    """
+
+    assert Day1.part_two(input) == 10
+  end
+
 end
