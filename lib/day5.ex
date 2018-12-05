05177766141408896bd4622be082dd4815759452
Brian Balser <brian.balser@gmail.com>
2018-12-05 15:47:00 -0500

    Day5 - part 2

--- lib/day5.ex
+++ lib/day5.ex
@@ -1,18 +1,40 @@
 defmodule Day5 do
   def part_one(input) do
     input
-    |> String.trim()
-    |> to_charlist()
+    |> create_polymer()
     |> remove_reactions([])
     |> Enum.count()
   end
 
-  defp remove_reactions([a, b | tail], []) when abs(a - b) == 32, do: remove_reactions(tail, [])
+  defp remove_reactions([a, b | tail], []) when abs(a - b) == 32 do
+    remove_reactions(tail, [])
+  end
 
-  defp remove_reactions([a, b | tail], [last | result]) when abs(a - b) == 32,
-    do: remove_reactions([last | tail], result)
+  defp remove_reactions([a, b | tail], [last | result]) when abs(a - b) == 32 do
+    remove_reactions([last | tail], result)
+  end
 
   defp remove_reactions([a, b | tail], result), do: remove_reactions([b | tail], [a | result])
   defp remove_reactions([], result), do: Enum.reverse(result)
   defp remove_reactions([a], result), do: Enum.reverse([a | result])
+
+  def part_two(input) do
+    polymer = create_polymer(input)
+
+    Stream.map(?A..?Z, fn unit -> remove_unit_type(polymer, unit) end)
+    |> Stream.map(fn polymer -> remove_reactions(polymer, []) end)
+    |> Stream.map(fn polymer -> Enum.count(polymer) end)
+    |> Enum.min()
+  end
+
+  defp remove_unit_type(polymer, unit) do
+    unit_type = [unit, unit + 32]
+    Enum.filter(polymer, fn x -> x not in unit_type end)
+  end
+
+  defp create_polymer(input) do
+    input
+    |> String.trim()
+    |> to_charlist()
+  end
 end
--- test/day5_test.exs
+++ test/day5_test.exs
@@ -6,4 +6,10 @@ defmodule Day5Tests do
 
     assert Day5.part_one(input) == 10
   end
+
+  test "part_two should more stuff" do
+    input = "dabAcCaCBAcCcaDA"
+
+    assert Day5.part_two(input) == 4
+  end
 end
